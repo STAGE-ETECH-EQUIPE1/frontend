f522133b82fbb21fa5b63c8d0833a217598e4750
mvjdev <hei.maria.625@gmail.com>
2025-07-16 13:00:29 +0300

    fix: format code with Test

--- src/__tests__/Home.test.tsx
+++ src/__tests__/Home.test.tsx
@@ -1,10 +1,10 @@
 import { render, screen } from '@testing-library/react'
-import Home from '../app/page'
+import HomePage from '@/app/[locale]/page'
 
 describe('Home page', () => {
   it('renders Hello world heading', () => {
-    render(<Home />)
-    const heading = screen.getByRole('heading', { name: 'Hello world All' })
+    render(<HomePage />)
+    const heading = screen.getByRole('heading')
     expect(heading).toBeInTheDocument()
   })
 })
